efb3a78a36b1f34cabd0241b51fda4a7b5bbade5
Hugo Escalpelo <hugoescalpelo@gmail.com>
2023-10-14 20:55:15 -0600

    face-recognition program updated

--- deepface/face-recognition.py
+++ deepface/face-recognition.py
@@ -1,14 +1,15 @@
 # Import Libraries
 from deepface import DeepFace
 import pandas as pd
+import json
 
 # Finding face
 print ("Finding face")
-
+df=pd.DataFrame();
 # df = DeepFace.find(img_path = "img1.jpg", db_path = "C:/workspace/my_db")
-df = DeepFace.find (img_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/faces/carrie1.png", db_path = "/home/hugo/Documents/GitHub/apertura-puertas-reconocimiento-facial/deepface/my_db", enforce_detection = "false")
+df = DeepFace.find (img_path = "/home/hugo/Pictures/picasso.png", db_path = "/home/hugo/Documents/GitHub/data-visualization/faces/deepface_db", enforce_detection = "true")
 print ("Results ")
 print (df)
 
-# print ("Seconday similitude")
-# print (df.identity[1])
+
+
